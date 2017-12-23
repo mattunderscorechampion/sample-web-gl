ff1e7dd2f86c73907a051c8d2d68a0918aeabd56
Matt Champion <mattunderscorechampion@gmail.com>
2017-12-23 18:33:20 +0000

    Rotate the cube on a cylindrical base.

--- sample.ts
+++ sample.ts
@@ -9,14 +9,21 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = -0.4;
 scene.add(cube);
+
+const baseGeometry = new THREE.CylinderGeometry(4, 4, 0.1, 50);
+const baseMaterial = new THREE.MeshBasicMaterial({ color: 0x0000bb });
+const base = new THREE.Mesh(baseGeometry, baseMaterial);
+base.position.y = -1;
+scene.add(base);
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
+    base.rotation.y += 0.01;
     cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
