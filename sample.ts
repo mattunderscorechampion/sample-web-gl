b2c32a1b37e7a4660183405da1cd9138bf754449
Matt Champion <mattunderscorechampion@gmail.com>
2017-12-24 18:08:51 +0000

    Use lights and lambertian materials.

--- sample.ts
+++ sample.ts
@@ -2,25 +2,32 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new THREE.OrbitControls(camera);
 
-const skyBoxGeometry = new THREE.BoxGeometry(100, 100, 100);
-const skyBoxMaterial = new THREE.MeshBasicMaterial({ color: 0xfafafa, side: THREE.BackSide });
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 50, 0);
+scene.add(light);
+
+const skyBoxGeometry = new THREE.SphereGeometry(100, 100, 100);
+const skyBoxMaterial = new THREE.MeshLambertMaterial({ color: 0xfafafa, side: THREE.BackSide });
 const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 scene.add(skyBox);
 
-const baseGeometry = new THREE.CylinderGeometry(4, 4, 0.1, 50);
-const baseMaterial = new THREE.MeshBasicMaterial({ color: 0x000099 });
+const baseGeometry = new THREE.CylinderGeometry(4, 4, 0.1, 100);
+const baseMaterial = new THREE.MeshLambertMaterial({ color: 0x000099 });
 const base = new THREE.Mesh(baseGeometry, baseMaterial);
 base.position.y = -1;
 scene.add(base);
 
-const outerHandleBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.6, 0.6, 0.2, 50)));
-const innerHandleBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.4, 0.4, 0.2, 50)));
+const outerHandleBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.6, 0.6, 0.2, 100)));
+const innerHandleBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.4, 0.4, 0.2, 100)));
 const handleBSP = outerHandleBSP.subtract(innerHandleBSP);
 const handle = handleBSP.toMesh();
 handle.rotateX(0.5 * Math.PI);
@@ -28,14 +35,14 @@ handle.translate(1, new THREE.Vector3(1, 0, 0));
 handle.translate(-0.2, new THREE.Vector3(0, 0, 1));
 const translatedHandleBSP = new ThreeBSP(handle);
 
-const outerMugBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(1, 0.9, 2, 50)));
-const innerMugBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.95, 0.85, 2, 50)));
+const outerMugBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(1, 0.9, 2, 100)));
+const innerMugBSP = new ThreeBSP(new THREE.Mesh(new THREE.CylinderGeometry(0.95, 0.85, 2, 100)));
 const mugBSP = outerMugBSP.union(translatedHandleBSP).subtract(innerMugBSP);
-const mug = mugBSP.toMesh(new THREE.MeshBasicMaterial({ color: 0x009900 }));
+const mug = mugBSP.toMesh(new THREE.MeshLambertMaterial({ color: 0x009900 }));
 scene.add(mug);
 
-const coffeeGeometry = new THREE.CylinderGeometry(0.95, 0.85, 1.9, 50);
-const coffeeMaterial = new THREE.MeshBasicMaterial({ color: 0x1a0800 });
+const coffeeGeometry = new THREE.CylinderGeometry(0.95, 0.85, 1.9, 100);
+const coffeeMaterial = new THREE.MeshLambertMaterial({ color: 0x1a0800 });
 const coffee = new THREE.Mesh(coffeeGeometry, coffeeMaterial);
 scene.add(coffee);
 
