2c4f1e140437bc2a7a299d42130c167d741f9936
Matt Champion <mattunderscorechampion@gmail.com>
2017-12-24 12:33:25 +0000

    Replace the cube with the nested cylinders.

--- sample.ts
+++ sample.ts
@@ -13,17 +13,21 @@ const skyBoxMaterial = new THREE.MeshBasicMaterial({ color: 0xfafafa, side: THRE
 const skyBox = new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
 scene.add(skyBox);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x009900 });
-const cube = new THREE.Mesh(geometry, material);
-cube.position.y = -0.4;
-scene.add(cube);
-
 const baseGeometry = new THREE.CylinderGeometry(4, 4, 0.1, 50);
 const baseMaterial = new THREE.MeshBasicMaterial({ color: 0x000099 });
 const base = new THREE.Mesh(baseGeometry, baseMaterial);
 base.position.y = -1;
 scene.add(base);
+
+const outerCylinderGeometry = new THREE.CylinderGeometry(1, 0.9, 2, 50);
+const outerMaterial = new THREE.MeshBasicMaterial({ color: 0x009900 });
+const outerCylinder = new THREE.Mesh(outerCylinderGeometry, outerMaterial);
+scene.add(outerCylinder);
+
+const innerCylinderGeometry = new THREE.CylinderGeometry(0.95, 0.85, 2.001, 50);
+const innerMaterial = new THREE.MeshBasicMaterial({ color: 0x331100 });
+const innerCylinder = new THREE.Mesh(innerCylinderGeometry, innerMaterial);
+scene.add(innerCylinder);
 
 camera.position.z = 5;
 controls.update();
@@ -32,7 +36,8 @@ function animate() {
     requestAnimationFrame(animate);
 
     base.rotation.y += 0.01;
-    cube.rotation.y += 0.01;
+    outerCylinder.rotation.y += 0.01;
+    innerCylinder.rotation.y += 0.01;
 
     controls.update();
     renderer.render(scene, camera);
