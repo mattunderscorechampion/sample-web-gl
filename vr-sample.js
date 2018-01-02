61600d1de9265c30166f6e3a4085922264844273
Matt Champion <mattunderscorechampion@gmail.com>
2018-01-02 21:30:36 +0000

    Commit the built VR sample code.

---
+++ vr-sample.js
@@ -0,0 +1,35 @@
+(function setupVRSample() {
+    var skyColour = 0xfafafa;
+    var objectColour = 0x9bb2ff;
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    function createLight() {
+        var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+        var light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.set(0, 50, 0);
+        return [ambientLight, light];
+    }
+    function createSkyBox() {
+        var skyBoxGeometry = new THREE.SphereGeometry(100, 100, 100);
+        var skyBoxMaterial = new THREE.MeshLambertMaterial({ color: skyColour, side: THREE.BackSide });
+        return new THREE.Mesh(skyBoxGeometry, skyBoxMaterial);
+    }
+    function createObject() {
+        var baseGeometry = new THREE.SphereGeometry(2, 100, 100);
+        var baseMaterial = new THREE.MeshLambertMaterial({ color: objectColour });
+        return new THREE.Mesh(baseGeometry, baseMaterial);
+    }
+    createLight().forEach(function (o) { return scene.add(o); });
+    scene.add(createSkyBox());
+    scene.add(createObject());
+    camera.position.z = 5;
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+}());
+//# sourceMappingURL=vr-sample.js.map
