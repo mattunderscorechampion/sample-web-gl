0c74ac9658f3f5bb5d043dd1156dbe826a43ec6c
Matt Champion <mattunderscorechampion@gmail.com>
2018-01-29 20:56:57 +0000

    Update the generated code.

--- vr-sample.js
+++ vr-sample.js
@@ -5,16 +5,27 @@
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.vr.enabled = true;
     document.body.appendChild(renderer.domElement);
-    function tryPresentAsVR() {
+    function showEnterVR(display) {
+        var button = document.createElement('button');
+        button.style.position = 'absolute';
+        button.style.top = '0px';
+        button.style.left = '0px';
+        button.textContent = 'Enter VR mode';
+        document.body.appendChild(button);
+        button.addEventListener('click', function (event) {
+            renderer.vr.setDevice(display);
+            display.requestPresent([{ source: renderer.domElement }]);
+        });
+    }
+    function detectVR() {
         if ('getVRDisplays' in navigator) {
             navigator.getVRDisplays().then(function (displays) {
                 if (displays.length > 0) {
                     console.log('VR device detected');
                     var display = displays[0];
-                    renderer.vr.enabled = true;
-                    renderer.vr.setDevice(display);
-                    display.requestPresent([{ source: renderer.domElement }]);
+                    showEnterVR(display);
                 }
                 else {
                     console.log('No VR devices detected');
@@ -46,10 +57,9 @@
     scene.add(createObject());
     camera.position.z = 5;
     function animate() {
-        requestAnimationFrame(animate);
         renderer.render(scene, camera);
     }
-    animate();
-    tryPresentAsVR();
+    renderer.animate(animate);
+    detectVR();
 }());
 //# sourceMappingURL=vr-sample.js.map
